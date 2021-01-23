9098d3467a958710ae95c6b65ad6ddec07130af1
Healer <healer0616@126.com>
2021-01-23 10:44:46 +0800

    读取配置文件类

--- config/read_config.py
+++ config/read_config.py
@@ -7,11 +7,18 @@
 
 
 from configparser import ConfigParser
+import os
 
 
 class ReadConfig:
-    def read_config(self, section, option, file_name=None):
+    def read_config(self, section, option, file_name):
         cf = ConfigParser()  # 实例化这个模块里的ConfigParser类
         cf.read(file_name, encoding='utf-8')  # 打开配置文件
-        res = eval(cf.get(section, option))
-        return res
+        result = cf.get(section, option)
+        return result
+
+
+if __name__ == '__main__':
+    root_dir = os.path.dirname(os.path.abspath("read_config.py"))  # 获取当前文件所在目录的上一级目录,绝对路径
+    res = ReadConfig().read_config(section='HTTP', option='host', file_name=os.path.join(root_dir, "config.ini"))
+    print(res)
