0e49d17f83d99e0c9a842ad3bf2c22e32ae9f73f
Healer <healer0616@126.com>
2021-01-26 10:28:47 +0800

    增加邮件配置

--- config/setting.py
+++ config/setting.py
@@ -21,16 +21,20 @@ timeout = "1.0"
 # [EMAIL]
 mail_host = "smtp.qq.com"
 # 发件人邮箱
-mail_user = "1175429380@qq.com"
+mail_sender = "1175429380@qq.com"
 # 发件人授权码
 mail_pass = "wpanjothgyvkhcif"
 # 端口号默认:25 SSL:465
 mail_port = 25
 # 收件人邮箱
-receiver = "healer0616@126.com"
+mail_receiver = "healer0616@126.com"
 # 主题
 subject = "主题"
 # 正文
 content = "正文"
+# 测试报告路径
+reports_path = "/Users/healer/Desktop/测试文件/测试图片/30.jpg"
+# 文件名称filename可以任意写，写什么名字，邮件中显示什么名字
+file_name = "自动化测试报告"
 testuser = "Someone"
 on_off = 1
